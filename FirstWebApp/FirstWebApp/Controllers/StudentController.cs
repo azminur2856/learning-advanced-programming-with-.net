14756705f862039ba72f5abd99c4842371f2ae62
AZMINUR RAHMAN <97751681+azminur2856@users.noreply.github.com>
2025-07-17 22:11:34 +0600

    Add Student MVC components and views
    Introduced StudentController and Student model to support student-related features. Added views for listing, creating, and redirecting students, as well as a custom layout (_MyLayout.cshtml). Updated Home/Index.cshtml to display TempData messages and modified project files to include new components.

---
+++ FirstWebApp/FirstWebApp/Controllers/StudentController.cs
@@ -0,0 +1,54 @@
+﻿using FirstWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstWebApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            ViewBag.MTitle = "Student List";
+
+            Student s1 = new Student()
+            {
+                Id = 1,
+                Name = "AZMINUR RAHMAN",
+                Email = "22-46588-1@student.aiub.edu",
+                Address = "Dhaka, Bangladesh"
+            };
+
+            Student s2 = new Student()
+            {
+                Id = 2,
+                Name = "ASHANUR RAHMAN",
+                Email = "ashanur@gmail.com",
+                Address = "Dhaka, Bangladesh"
+            };
+
+            List<Student> students = new List<Student>();
+            students.Add(s1);
+            students.Add(s2);
+
+            return View(students);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.MTitle = "Create Student";
+            return View();
+        }
+
+        public ActionResult CheckRedirect()
+        {
+            TempData["Message"] = "This is a message from Student Controller Check Redirect";
+            //return RedirectToAction("Index", "Home"); // This will redirect to the Index action of the Home controller
+            //return RedirectToAction("Index"); // This will redirect to the Index action of the current controller
+            return Redirect("http://www.aiub.edu"); // This will redirect to an external URL
+        }
+    }
+}

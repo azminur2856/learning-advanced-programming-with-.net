40f21707ae5bdf776965afdd2ba50761c67ac3f2
AZMINUR RAHMAN <97751681+azminur2856@users.noreply.github.com>
2025-07-17 21:28:27 +0600

    Intro MVC

---
+++ FirstWebApp/FirstWebApp/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Bio()
+        {
+            ViewBag.Message = "Your bio page.";
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            ViewBag.Message = "Your education page.";
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            ViewBag.Message = "Your project page.";
+            return View();
+        }
+
+        public ActionResult References()
+        {
+            ViewBag.Message = "Your references page.";
+            return View();
+        }
+    }
+}

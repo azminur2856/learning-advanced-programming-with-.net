65d44911d653fd1ce61f8b8d38776caed51266e5
AZMINUR RAHMAN <97751681+azminur2856@users.noreply.github.com>
2025-09-07 14:47:52 +0600

    IntroTrierArchi

---
+++ IntroTierArchitecture/BLL/Services/StudentService.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using BLL.DTOs;
+using DAL.EF.Tables;
+using DAL.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class StudentService
+    {
+        public static Mapper GetMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Student, StudentDTO>().ReverseMap();
+            });
+            return new Mapper(config);
+        }
+
+        public static List<StudentDTO> Get()
+        {
+            var data = new StudentRepo().Get();
+            return GetMapper().Map<List<StudentDTO>>(data);
+        }
+
+        public static bool Create(StudentDTO s)
+        {
+            var st = GetMapper().Map<Student>(s);
+            return new StudentRepo().Create(st);
+        }
+    }
+}
---
+++ IntroTierArchitecture/DAL/Repos/StudentRepo.cs
@@ -0,0 +1,49 @@
+﻿using DAL.EF;
+using DAL.EF.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    public class StudentRepo
+    {
+        UMSContext db;
+        public StudentRepo()
+        {
+            db = new UMSContext();
+        }
+
+        public bool Create(Student s)
+        {
+            db.Students.Add(s);
+            return db.SaveChanges() > 0;
+        }
+
+        public List<Student> Get()
+        {
+            return db.Students.ToList();
+        }
+
+        public Student Get(int id)
+        {
+            return db.Students.Find(id);
+        }
+
+        public bool Update(Student s)
+        {
+            var exobj = Get(s.Id);
+            db.Entry(exobj).CurrentValues.SetValues(s);// It will change all from s if any property comes with null or default value then it change thet so need to be carefull before use it
+            return db.SaveChanges() > 0;
+        }
+
+        public bool Delete(int id)
+        {
+            var exobj = Get(id);
+            db.Students.Remove(exobj);
+            return db.SaveChanges() > 0;
+        }
+    }
+}
